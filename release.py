119041337aef372736d809d42a37596a89e61108
Mateusz Grzeliński <grzelinskimat@gmail.com>
2023-05-03 16:32:35 +0200

    Reorganize files

---
+++ release.py
@@ -0,0 +1,38 @@
+from zipfile import ZipFile
+import os
+from os.path import basename
+import os
+import re
+import logging
+import pathlib
+
+logging.basicConfig(level=logging.INFO)
+
+ACCEPTED_PATTERNS = tuple(
+    re.compile(pattern) for pattern in (r".*\.py$", r".*\.blend$", r".*\.md$")
+)
+EXCLUDED_PATTERNS = tuple(re.compile(pattern) for pattern in (r".*/__pycache__/.*", ".*/.vscode/.*"))
+
+
+TAG = os.getenv("GITHUB_REF")
+if TAG is None:
+    from kuba_addon import bl_info
+
+    TAG = "v" + ".".join(str(v) for v in bl_info["version"])
+
+if __name__ == "__main__":
+    with ZipFile(f"kuba-addon-{TAG}.zip", "w") as zipObj:
+        for folderName, subfolders, filenames in os.walk("kuba_addon"):
+            for filename in filenames:
+                # create complete filepath of file in directory
+                filePath = os.path.join(folderName, filename)
+                filePath = pathlib.Path(filePath).as_posix()
+                is_exluded = any(pat.match(filePath) for pat in EXCLUDED_PATTERNS)
+                if is_exluded:
+                    continue
+                is_accepted = any(pat.match(filePath) for pat in ACCEPTED_PATTERNS)
+                if not is_accepted:
+                    continue
+                logging.info(f"zipping: {filePath}")
+                # Add file to zip
+                zipObj.write(filePath, filePath)
